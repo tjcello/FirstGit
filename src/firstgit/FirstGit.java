967ce0e65ccc076f8b525138d3f35cab65ffb95c
tduwe <tduwe@L11710042773.wctc.msft>
2015-09-01 16:33:11 -0500

    Added second message to main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -16,6 +16,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Welcome Java");
     }
     
 }
